86b9ab52a8412af1d90a9f3cb3cc27a7e5c76aee
Petr Mácha <91247806+PetrMacha021@users.noreply.github.com>
2021-11-28 23:03:55 +0100

    added code

--- main.ts
+++ main.ts
@@ -1,3 +1,40 @@
-basic.forever(function () {
-	
-})
+let time = 1500;
+let timePressed = 0;
+
+input.onButtonPressed(Button.B, () => {
+    time = randint(1, 12) * 250;
+    music.playTone(Note.C, time);
+});
+
+input.onButtonPressed(Button.A, () => {
+    music.playTone(Note.C, time);
+    basic.pause(250);
+});
+
+input.onLogoEvent(TouchButtonEvent.Touched, () => {
+    timePressed = control.millis();
+});
+
+input.onLogoEvent(TouchButtonEvent.Released, () => {
+    let timeDifference = Math.abs((control.millis() - timePressed) - time);
+    serial.writeLine(timeDifference.toString());
+    if (timeDifference <= 250) {
+        basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+        soundExpression.giggle.playUntilDone();
+    } else {
+        basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+        soundExpression.sad.playUntilDone();
+    }
+});
